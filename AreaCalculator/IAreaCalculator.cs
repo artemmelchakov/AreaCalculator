6a608f011e98091bbded13381211c43aec20fd0e
Артём Мельчаков <maw@pbprog.ru>
2024-01-18 19:28:16 +0300

    Add project files.

---
+++ AreaCalculator/IAreaCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace AreaCalculator
+{
+    /// <summary>
+    /// Интерфейс, реализующий методы, занимающиеся вычислением площади фигуры.
+    /// </summary>
+    public interface IAreaCalculator
+    {
+        /// <summary>
+        /// Вычислить площадь фигуры.
+        /// </summary>
+        /// <returns> Площадь фигуры. </returns>
+        abstract double Calculate();
+    }
+}
